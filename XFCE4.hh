7364936f992b1e0a59074f47a60afe685a1daa2d
Guide26 <116459685+Guide26@users.noreply.github.com>
2022-10-22 15:13:47 -0400

    XFCE4.hh
    Hope This Was Helpful, 👍

---
+++ XFCE4.hh
@@ -0,0 +1,6 @@
+If you're just here to use crouton, you can grab the Version Imgur from https://goo.gl/fd3zc. Download it, pop open a shell (Ctrl+Alt+T, type shell and hit enter), make the installer executable with sudo install -Dt /usr/local/bin -m 755 ~/Downloads/crouton, then launch it with sudo crouton to see the help text. See the "examples" section for some usage examples.
+Then Type  sudo startxfce4.
+Fix A broken laptop is pretty hard but theres away to fix without spending money (REQUIRD INSTRUTION) Screw DRiver Tools that can fix wires or something
+Okay Now Go to the back of the laptop ya am talking about a chromebook now screw the bottom of the chromebook you will see this little thing then take it out and bring it to the shop where they give you a new on 
+But Crack screen thats gonna have to go to the shop,
+I Hope this was Helpful,
